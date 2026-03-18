858e40d21ff56d612f36479c2bcf4129025f2428
ASapAbro <143710790+ASapAbro@users.noreply.github.com>
2026-03-18 19:41:40 +0100

    Partie 2 POO: ajout et affichage

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+# Ajouter et afficher des livres
+livres = []
+def ajouter_livre():
+    titre = input("titre :")
+    auteur = input("Auteur :")
+    livres.append({"titre": titre, "auteur": auteur})
+    print(f"'{titre}' à été ajouté.")
+
+
+def afficher_livres():
+    if not livres:
+        print("Aucun livre enregistré.")
+        return
+    print("\n--- Liste des livres ---")
+    for i, livre in enumerate(livres, 1):
+        print(f"{i}. {livre['titre']} — {livre['auteur']}")
+
+
+# menu Itératif
+
 def afficher_menu():
     print("\n===== Bibliothèque =====")
     print("1. Ajouter un livre")
@@ -7,10 +27,17 @@ def afficher_menu():
     print("0. Quitter")
     return input("Votre choix : ")
 
+
+#point d'entré
+
 def main():
     while True:
         choix = afficher_menu()
-        if choix == "0":
+        if choix == "1":
+            ajouter_livre()
+        elif choix == "2":
+            afficher_livres()
+        elif choix == "0":
             print("Au revoir")
             break
         else:
